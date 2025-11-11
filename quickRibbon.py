33ba7971685afbee942ff9bfe66583014db009c4
Linqi Sun <lsa172@sfu.ca>
2025-11-11 00:01:40 -0800

    Update quickRibbon.py

--- quickRibbon.py
+++ quickRibbon.py
@@ -1,6 +1,15 @@
+'''
+QuickRibbon: a tool to automate ribbon setups
+Created by Linqi "Lyne" Sun under the supervision of Philippe Pasquier at Simon Fraser University 
+for class IAT806: Interdisciplinary Design Approaches to Computing
+'''
+
 import maya.cmds as cmds
 import maya.mel as mel
 import sys
+import statistics
+import math
+
 
 class quickRibbon(object):
     
@@ -40,6 +49,10 @@ class quickRibbon(object):
         cmds.button(label="Finalize NURBS Plane", command= self.make_plane)
         cmds.separator(height=15, style='none')
         cmds.button(label="Finalize Ribbon", command= self.make_ribbon);#incomplete button. Main function isn't here yet
+
+        #UI: user notice text
+        cmds.separator(height=15, style='none')
+        cmds.text("P.S. Please keep this window open while editing the NURBS plane for your ribbon", align='center')
         
         #display new window
         cmds.showWindow()
@@ -48,7 +61,7 @@ class quickRibbon(object):
         #function to create the NURBS plane
         #find the axis
         self.axisChoice = cmds.radioButtonGrp(self.radioValue, q=True, sl=True)
-        self.axisArray = [[1.0,0.0,0.0,], [0.0,1.0,0.0], [0.0,0.0,1.0]]
+        self.axisArray = [[1.0,0.0,0.0], [0.0,1.0,0.0], [0.0,0.0,1.0]]
         self.axis = self.axisArray[self.axisChoice-1]
         #find the width and length
         self.nurbsW = cmds.floatFieldGrp(self.w, query=True, value1=True)
@@ -80,7 +93,7 @@ class quickRibbon(object):
                  fol_subfix = str(everyItem+1)
              
                  ##create a follicle
-                 fol_shp=cmds.createNode('follicle', n=self.nurbsN+'flc_Shape'+fol_subfix) #returns follicle shape node
+                 fol_shp=cmds.createNode('follicle', n=self.nurbsN+'ribbon_flc_Shape'+fol_subfix) #returns follicle shape node
                  cmds.setAttr(fol_shp+'.simulationMethod', 0)
                  ###find the follicle transform node
                  fol_trns = cmds.pickWalk(d='up') #returns a list that contains follicle transform node
@@ -100,22 +113,69 @@ class quickRibbon(object):
                     cmds.setAttr(fol_shp+'.parameterV', fol_uv_x)
                     fol_uv_x +=fol_uv_delta
                  ###create an offset group beneath the follicle
-                 fol_offset = cmds.group(em=True, n=self.nurbsN+'flc_offset_'+fol_subfix)
+                 fol_offset = cmds.group(em=True, n=self.nurbsN+'ribbon_flc_offset_'+fol_subfix)
                  cmds.parent(fol_offset, fol_trns[0], r=True)
                  ###create a follicle joint
-                 cmds.joint(rad=0.3, n=self.nurbsN+'flc_jnt_'+fol_subfix)#automatically parented under offset
+                 cmds.joint(rad=0.3, n=self.nurbsN+'ribbon_flc_jnt_'+fol_subfix)#automatically parented under offset
         
             ##group all created follicles
-            all_fol=cmds.ls(self.nurbsN+'flc*', type='follicle')
-            cmds.group(all_fol, n=self.nurbsN+'follicle_grp')
+            all_fol = cmds.ls(self.nurbsN+'ribbon_flc_*', type='follicle')
+            cmds.group(all_fol, n=self.nurbsN+'ribbon_flc_grp')
 
-            #create controller joints
+            ##create controller joints and controllers
             
-        
+            ##define function to make ctls
+            def make_ctl(ctName, ctjnt):
+                mct_axisArray = [[0.0,0.0,1.0], [1.0,0.0,0.0], [1.0,0.0,0.0]]
+                mct_axis = mct_axisArray[self.axisChoice-1]
+                mct_loc = cmds.xform(ctjnt, q=True, t=True, ws=True)
+                mct_proto = cmds.circle(nr=mct_axis, r=1.2, ch=False, n=self.nurbsN+ctName, c=mct_loc)
+                cmds.matchTransform(mct_proto, ctjnt, piv=True)
+                return mct_proto
 
-        #create controllers and parent-constrain controller joints
+            ##create ctl joint at the first follicle joint
+            loc_folJnt_01 = cmds.xform(self.nurbsN+'ribbon_flc_jnt_1', q=True, t=True, ws=True)
+            cmds.select(cl=True)
+            ctJnt_01 = cmds.joint(rad=1.0, n=self.nurbsN+'ribbon_control_jnt_1', p=loc_folJnt_01)
+            ##create the first ctl and parent-constrain the first ctl joint
+            ct_01 = make_ctl('ribbon_ctl_1', ctJnt_01)
+            cmds.parentConstraint(ct_01, ctJnt_01)
+            
+            ##create ctl joint at the last follicle joint
+            ctJnt_count = cmds.intFieldGrp(self.ctlCount, q=True, value1=True)
+            loc_folJnt_last = cmds.xform(self.nurbsN+'ribbon_flc_jnt_'+str(fol_i+1), q=True, t=True, ws=True)
+            cmds.select(cl=True)
+            ctJnt_end = cmds.joint(rad=1.0, n=self.nurbsN+'ribbon_control_jnt_'+str(ctJnt_count), p=loc_folJnt_last)
+            ##create the last ctl and parent-constrain the last ctl joint
+            ct_end = make_ctl('ribbon_ctl_'+str(ctJnt_count), ctJnt_end)
+            cmds.parentConstraint(ct_end, ctJnt_end)
+            
+            ##define all the indexes needed for calculating middle ctl joint locations here
+            ctJnt_pcDelta = 1/(ctJnt_count-1)#increments for point constraint weights from first and last joint on middle joints
+            ctJnt_list = list(range(2, ctJnt_count))
+            
+            ##create ctl joints in the middle
+            for everyJoint in ctJnt_list:
+                ###create the ctl joint
+                ctJnt_mid_subfix = str(everyJoint)
+                cmds.select(cl=True)
+                ctJnt_mid = cmds.joint(rad=1.0, n=self.nurbsN+'ribbon_control_jnt_'+ctJnt_mid_subfix)
+                ###place the joint with point constraint and then delete the point constraint
+                cmds.pointConstraint(ctJnt_01, ctJnt_end, ctJnt_mid)
+                ctJnt_pc1 = cmds.pointConstraint(ctJnt_01, ctJnt_mid, e=True, w=ctJnt_pcDelta*(everyJoint-1))
+                ctJnt_pc2 = cmds.pointConstraint(ctJnt_end, ctJnt_mid, e=True, w=1-ctJnt_pcDelta*(everyJoint-1))
+                cmds.delete(ctJnt_pc1,ctJnt_pc2)
+                ###create the ctl and parent-constrain ctl joints
+                ct_mid = make_ctl('ribbon_ctl_'+ctJnt_mid_subfix, ctJnt_mid)
+                cmds.parentConstraint(ct_mid, ctJnt_mid)
+            
+            ##skinbind controller joints to the NURBS plane
+            all_ctJnts = cmds.ls(self.nurbsN+'ribbon_control_jnt_*', type='joint')
+            cmds.skinCluster(all_ctJnts, self.ribbon)
 
-        #skinbind controller joints to the NURBS plane
+            
+
+
 
         #point-constrain the middle controller
 
